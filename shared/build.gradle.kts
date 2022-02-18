5a03d08a5bc70c962cdc1a778ba20753b0bd5c2e
Chris Russell <40370458+ninehundreds@users.noreply.github.com>
2022-02-18 13:53:49 -0500

    Updated shared module to use buildSrc. Added exports for network module, so it's visible in the iOS client.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -13,11 +13,19 @@ kotlin {
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
+
+            export(project(":network")) {
+                isStatic = true
+            }
         }
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                api(project(":network"))
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -28,7 +36,7 @@ kotlin {
         val androidTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
-                implementation("junit:junit:4.13.2")
+                implementation(Deps.junit)
             }
         }
         val iosX64Main by getting
@@ -53,10 +61,10 @@ kotlin {
 }
 
 android {
-    compileSdk = 31
+    compileSdk = ConfigData.compileSdkVersion
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
-        minSdk = 21
-        targetSdk = 31
+        minSdk = ConfigData.minSdkVersion
+        targetSdk = ConfigData.targetSdkVersion
     }
 }
